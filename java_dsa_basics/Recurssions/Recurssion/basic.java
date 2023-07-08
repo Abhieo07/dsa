56a2418bb20b7f6243eb99f139e2a09c9469e8c7
Abhieo07 <gautam.mandal@vit.edu.in>
2023-07-08 23:40:45 +0530

    day 5 directory changes

---
+++ java_dsa_basics/Recurssions/Recurssion/basic.java
@@ -0,0 +1,26 @@
+package Recurssions.Recurssion;
+
+import java.util.Scanner;
+
+public class basic {
+    static void printIncreasing(int n){  
+        // base case
+        if(n == 1){
+            System.out.println(n);
+            return;
+        }
+        // assume it will print 1,2,3,,,n-1
+        printIncreasing(n - 1);
+        //sout(n)
+        System.out.println(n);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter n");
+        int n = sc.nextInt();
+
+        printIncreasing(n);
+
+    }    
+}
