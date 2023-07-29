2e5b000d260b27257a3b0435f76aba20231330f2
Abhieo07 <gautam.mandal@vit.edu.in>
2023-07-30 02:32:49 +0530

    comit

---
+++ java_dsa_basics/collections/ListInterfaceExamples.java
@@ -0,0 +1,18 @@
+package collections;
+
+import java.util.*;
+
+public class ListInterfaceExamples {
+    static void ArrayListExamples(){
+        ArrayList<Integer> l = new ArrayList<>();
+        l.add(1);
+        l.add(2);
+        l.add(3);
+        System.out.println(l);
+        l.set(1, 10);
+        System.out.println(l);
+    }
+    public static void main(String[] args) {
+        
+    }
+}
